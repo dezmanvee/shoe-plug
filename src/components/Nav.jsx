59fd7abc6d6b55e34c7b0cc60818f0e759e3e6d5
Bamidele Ademokoya <bamyonline@gmail.com>
2023-10-08 12:47:42 +0100

    Added toggle functionality for mobile devices

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,34 +1,77 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
 import { navLinks } from "../constants";
+import { useState } from "react";
+import { GrClose } from "react-icons/gr";
 
 const Nav = () => {
+  const [mobileToggle, setMobileToggle] = useState(true);
+  
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
-      <nav className="flex justify-between items-center max-container">
+    <header
+      id="navHeader"
+      className="padding-x py-8 fixed z-10 w-full bg-white"
+    >
+      <nav className="flex justify-between items-center max-container max-lg:h-auto max-lg:items-start max-lg:relative">
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-          {navLinks.map((link) => {
-            const { href, label } = link;
-            return (
-              <li key={label}>
-                <a href={href}
-                className="font-montserrat leading-normal text-lg text-slate-gray">{label}</a>
-              </li>
-            );
-          })}
+        <ul
+          className={`max-lg:bg-black max-lg:bg-opacity-70 max-lg:bg-blend-darken max-lg:z-10 max-lg:w-full max-lg:h-screen max-lg:p-8 max-lg:fixed right-0 top-0 left-0 bottom-0 ${
+            mobileToggle ? "max-lg:hidden" : "max-lg:block"
+          }`}
+        >
+          <div className="flex-1 flex justify-center items-center gap-16 max-lg:flex-col max-lg:justify-start max-lg:items-start max-lg:bg-white max-lg:fixed max-lg:w-full max-lg:gap-12 max-lg:p-8 max-lg:h-full right-0 top-0">
+            {navLinks.map((link) => {
+              const { href, label } = link;
+              return (
+                <li key={label}>
+                  <a
+                    href={href}
+                    className="font-montserrat leading-normal text-lg text-slate-gray max-lg:font-bold"
+                  >
+                    {label}
+                  </a>
+                </li>
+              );
+            })}
+          </div>
         </ul>
         <div className="font-montserrat leading-normal text-lg max-lg:hidden">
-            <a href="/">
-                Sign in / Explore now
-            </a>
+          <a href="/">Sign in / Explore now</a>
         </div>
-        <div className="hidden max-lg:block">
-            <img src={hamburger} alt="Hamburger"
+        {/* hamburger */}
+        <div
+          key={1}
+          className={`hidden z-10 cursor-pointer ${
+            mobileToggle === false ? "max-lg:hidden" : "max-lg:block"
+          }`}
+        >
+          <img
+            src={hamburger}
+            alt="Hamburger"
             width={25}
-            height={25} />
+            height={25}
+            onClick={() => {
+              setMobileToggle(!mobileToggle);
+            }}
+          />
+        </div>
+
+        {/* close */}
+        <div
+          key={2}
+          className={`hidden z-10 cursor-pointer ${
+            mobileToggle === true ? "max-lg:hidden" : "max-lg:block"
+          }`}
+        >
+          <p
+            onClick={() => {
+              setMobileToggle(!mobileToggle);
+            }}
+          >
+            <GrClose className="text-3xl" />
+          </p>
         </div>
       </nav>
     </header>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
           Our summer collections
         </p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 xl:rounded-2xl">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 xl:rounded-2xl max-lg:-z-10">
             The New Arrival
           </span>
           <br />
@@ -48,7 +48,7 @@ const Hero = () => {
           alt="Shoe collection"
           width={610}
           height={500}
-          className="object-contain z-10 relative"
+          className="object-contain z-9 relative"
         />
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) => {
